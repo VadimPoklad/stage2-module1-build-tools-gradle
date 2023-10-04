938d7049842965dbccc511254caeb5a17b684f25
Vadym Poklad <vadik.pokladd@gmail.com>
2023-10-04 19:07:37 +0300

    Answ

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,14 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        if (org.apache.commons.lang3.StringUtils.isEmpty(str)) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(str);
+            return (d>=0);
+        }catch (NumberFormatException e){
+            return false;
+        }
     }
 }
